dbb41f22bedbcc062436aec089b628daba2b9e98
ishibashihiroto1196 <ishibashihiroto1196@DESKTOP-7U8EO9U.ad.se-project.co.jp>
2020-10-08 09:54:20 +0900

    石橋  コミット

--- carfare/src/common/CommonDB.java
+++ carfare/src/common/CommonDB.java
@@ -16,16 +16,14 @@ public class CommonDB {
 	 *	登録、編集画面でプルダウンメニュー作成時に使用
 	 *	片道or往復
 	 **/
-	public ResultSet getRouteAll() {
+	public static ResultSet getRouteAll() {
 		try {
 			//DBへ接続
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			Statement stmt = connect.createStatement();
 			String getQuery = "SELECT * FROM route ORDER BY route_no ASC;";
-			ResultSet rs = stmt.executeQuery(getQuery);
-
-			return rs;
+			return stmt.executeQuery(getQuery);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -38,15 +36,14 @@ public class CommonDB {
 	 *	登録、編集画面でプルダウンメニュー作成時に使用
 	 *	交通手段
 	 **/
-	public ResultSet getTransitAll() {
+	public static ResultSet getTransitAll() {
 		try {
 			//DBへ接続
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			Statement stmt = connect.createStatement();
 			String getQuery = "SELECT * FROM transit ORDER BY transit_no ASC;";
-			ResultSet rs = stmt.executeQuery(getQuery);
-			return rs;
+			return stmt.executeQuery(getQuery);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -59,27 +56,29 @@ public class CommonDB {
 	 *	登録、編集画面から使用
 	 *	交通機関、出発駅、到着駅を受け取り、LIKE句をかけて絞り込む
 	 **/
-	public ResultSet getTransitDataAll(String transit_no, String from_st, String to_st) {
+	public static ResultSet getTransitDataAll(String transit_no, String from_st, String to_st, int limitSta) {
 		try {
 			/**
 			 * 受け取った値の確認
 			 * 受け取った値がnullであれば空文字("")を代入
 			 * そうでなければそのまま代入する
 			 **/
-			String transitNo = (transit_no == null) ? "" : "'%" + transit_no + "%'";
-			String fromSt = (from_st == null) ? "" : "'%" + from_st + "%'";
-			String toSt = (to_st == null) ? "" : "'%" + to_st + "%'";
+			transit_no = (transit_no == null) ? "'%%' " : "'%" + transit_no + "%' ";
+			from_st = (from_st == null) ? "'%%' " : "'%" + from_st + "%' ";
+			to_st = (to_st == null) ? "'%%' " : "'%" + to_st + "%' ";
 			//DBへ接続
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			Statement stmt = connect.createStatement();
-			String getQuery = "SELECT * FROM transit_data"
-					+ " WEHRE transit_no LIKE " + transitNo
-					+ " AND from_st LIKE " + fromSt
-					+ " AND to_st LIKE " + toSt
-					+ " ORDER BY data_id ASC;";
-			ResultSet rs = stmt.executeQuery(getQuery);
-			return rs;
+			String getQuery = "SELECT transit_data.data_id,transit_data.transit_no,transit.transit_name,transit_data.from_st,transit_data.to_st,transit_data.price "
+					+ "FROM transit_data , transit "
+					+ "WHERE transit_data.transit_no = transit.transit_no "
+					+ "AND transit_data.transit_no LIKE " + transit_no
+					+ "AND transit_data.from_st LIKE " + from_st
+					+ "AND transit_data.to_st LIKE " + to_st
+					+ "ORDER BY transit_data.data_id ASC "
+					+ "LIMIT " + limitSta + " , 10;";
+			return stmt.executeQuery(getQuery);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -89,17 +88,58 @@ public class CommonDB {
 	}
 
 	/**
+	 *	登録、編集画面から使用
+	 *	交通機関、出発駅、到着駅を受け取り、LIKE句をかけて絞り込む
+	 *	絞り込んだ件数を返す
+	 **/
+	public static int getTransitDataCnt(String transit_no, String from_st, String to_st) {
+		try {
+			/**
+			 * 受け取った値の確認
+			 * 受け取った値がnullであれば空文字("")を代入
+			 * そうでなければそのまま代入する
+			 **/
+			transit_no = (transit_no == null) ? "'%%' " : "'%" + transit_no + "%' ";
+			from_st = (from_st == null) ? "'%%' " : "'%" + from_st + "%' ";
+			to_st = (to_st == null) ? "'%%' " : "'%" + to_st + "%' ";
+			//DBへ接続
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			Statement stmt = connect.createStatement();
+			String getQuery = "SELECT count(*) AS count "
+					+ "FROM transit_data , transit "
+					+ "WHERE transit_data.transit_no = transit.transit_no "
+					+ "AND transit_data.transit_no LIKE " + transit_no
+					+ "AND transit_data.from_st LIKE " + from_st
+					+ "AND transit_data.to_st LIKE " + to_st
+					+ "ORDER BY transit_data.data_id ASC;";
+			ResultSet rs = stmt.executeQuery(getQuery);
+			rs.next();
+			return rs.getInt("count");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	/**
 	 *	一覧画面で使用
 	 **/
-	public ResultSet getTransitListAll() {
+	public static ResultSet getTransitListAll() {
 		try {
 			//DBへ接続
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			Statement stmt = connect.createStatement();
-			String getQuery = "SELECT * FROM transit_list ORDER BY day ASC;";
-			ResultSet rs = stmt.executeQuery(getQuery);
-			return rs;
+			String getQuery = "SELECT transit_list.id,transit_list.day,transit.transit_name,route.route_name, " +
+					"transit_list.from_st,transit_list.to_st,transit_list.price " +
+					"FROM transit_list,route,transit " +
+					"WHERE transit_list.route_no = route.route_no " +
+					"AND transit_list.transit_no = transit.transit_no " +
+					"ORDER BY day ASC;";
+			return stmt.executeQuery(getQuery);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -114,16 +154,15 @@ public class CommonDB {
 	 *	一致するものがあれば ユーザー情報を返す
 	 *	一致するものがなければ nullを返す
 	 **/
-	public ResultSet getUser(String address, String password) {
+	public static ResultSet getUser(String address, String password) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			Statement stmt = connect.createStatement();
-			String getQuery = "SELECT * FROM user"
-					+ " WHERE address LIKE " + address
-					+ " AND password LIKE " + password + ";";
-			ResultSet rs = stmt.executeQuery(getQuery);
-			return rs;
+			String getQuery = "SELECT * FROM user "
+					+ "WHERE address LIKE '" + address + "' "
+					+ "AND password LIKE '" + password + "';";
+			return stmt.executeQuery(getQuery);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -138,9 +177,9 @@ public class CommonDB {
 	 *	一致するものがあれば trueを返す
 	 *	一致するものがなければ falseを返す
 	 **/
-	public Boolean isUser(String address, String password) {
+	public static Boolean isUser(String address, String password) {
 		try {
-			return getUser(address, password).isLast();
+			return getUser(address, password).next();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -151,15 +190,14 @@ public class CommonDB {
 	 *	ログイン画面で使用
 	 *	利用者分類表を返す
 	 **/
-	public ResultSet getUserCategoryAll() {
+	public static ResultSet getUserCategoryAll() {
 		try {
 			//DBへ接続
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 			Statement stmt = connect.createStatement();
 			String getQuery = "SELECT * FROM user_category ORDER BY role_id ASC;";
-			ResultSet rs = stmt.executeQuery(getQuery);
-			return rs;
+			return stmt.executeQuery(getQuery);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
