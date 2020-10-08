60ca6ffb31317a0f10c349f56f02dc957e92b502
harashinya1192 <harashinya1192@host.docker.internal>
2020-10-08 17:19:06 +0900

    new

--- carfare/src/login/Login.java
+++ carfare/src/login/Login.java
@@ -93,54 +93,6 @@ public class Login extends HttpServlet {
 	    getServletContext().getRequestDispatcher("/logincheck.jsp").forward(request, response);
 	}
 
-	/*
-	 *
-	 * パスワードをハッシュ化し、その値を比較してログイン可能かを判断する
-	 * これをベースに形にしていく予定です
-	 *
-	 *  現在はcommonDBを使用せずLogin.Javanにコード書きこんでます（石橋君と用打ち合わせして実際はcommonDBを実装しての処理になります！）
-	 *
-	 *
-	 *  【ログイン処理】
-	 *
-	 *  １.ログイン画面で入力したメアドを使い、UserテーブルからそのユーザーのID、ハッシュ化したパスワード、ソルトを持ってくる
-	 *
-	 *  ２.ログイン画面で入力したパスワードを、DBから取得したソルトを使いハッシュ化
-	 *
-	 *  ３.ログイン画面で入力したパスワードから生成されたハッシュ化した値と、
-	 *     DBに格納されているpasswordの値を比較しログイン可能かを判断
-	 *
-	 *  ４.ログイン可能ならユーザーIDを一覧画面などに引き継ぐ（ロールIDなども引きつぐ？）
-	 *
-	 * 【もし、登録機能を作らなければいけなくなった場合】
-	 * saltはaddressかuser_idをもとに乱数を生成しuserテーブルのsaltに格納
-	 * また、パスワードを先ほど生成したsaltを使いハッシュ化して格納
-	 * この処理でユーザーによって同じパスワードを使ったとしても生成されるハッシュ値が異なるようになる
-	 *
-	 *
-	 * 	何か不明なところや指摘などがあればおねがいします
-	 *
-	 *
-	 * 【その他】
-	 * userテーブルのaddressは一意にしたほうがいいかもしれないです
-	 *
-	 *
-	 * userテーブルのPasswordはハッシュ化するため桁数が足りなくなりそうですので、
-	 * まだ何桁までにするとは決めてないのでサンプルのハッシュ値を格納できる範囲に広げてください
-	 *
-	 * userテーブルにsaltを格納するカラムを追加願いします（設計書のほうは自分のほうで書き換えておきます）
-	 * こちらも同じく何桁になるかわからないので（略
-	 *
-	 * 【userのサンプルです】
-	 *	INSERT INTO `user`(`user_id`, `address`, `password`, `role_id`, `salt`)
-	 *	VALUES (1,"harashinya1192","11e95bc230ec51789ca5b027c5d52afecfba66fb3a150d63205d152370bb7d35",1,
-	 *	"88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589")
-	 *
-	 *	【ログイン情報】
-	 *	add   harashinya1192
-	 *  pass  hara1192
-	 *
-	 */
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
