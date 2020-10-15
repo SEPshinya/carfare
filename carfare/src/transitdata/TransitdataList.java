fa2b7efbf01c8f43cf20b124181520bae8c0284b
Ayano Onuma <onumaayano1199@se-project.co.jp>
2020-10-15 14:22:28 +0900

    交通手段一覧、ページング修正

--- carfare/src/transitdata/TransitdataList.java
+++ carfare/src/transitdata/TransitdataList.java
@@ -51,22 +51,10 @@ public class TransitdataList extends HttpServlet {
 		/** 入力値引継ぎ**/
 		String day=request.getParameter("day");
 		String route_no=request.getParameter("route_no");
-		String route_name=request.getParameter("route_name");
-		String transit_name =request.getParameter("transit_name");
 		String price=request.getParameter("price");
 
-		/** エンコードからデコード **/
-		//片道往復name
-		String route_name_encoded=request.getParameter("route_name_encoded");
-		if(route_name_encoded!=null) {
-			route_name= URLDecoder.decode(route_name_encoded, "UTF-8");
-		}
+		String id=request.getParameter("id");
 
-		//交通機関name
-		String transit_name_encoded =request.getParameter("transit_name_encoded");
-		if(transit_name_encoded!=null) {
-			transit_name= URLDecoder.decode(transit_name_encoded, "UTF-8");
-		}
 
 
 		/** ページング **/
@@ -144,8 +132,8 @@ public class TransitdataList extends HttpServlet {
 		//入力値引継ぎ
 		request.setAttribute("day",day);
 		request.setAttribute("route_no", route_no);
-		request.setAttribute("route_name", route_name);
-		request.setAttribute("transit_name", transit_name);
+		request.setAttribute("id", id);
+
 
 		//ページング関連
 		String listC = String.valueOf(listCnt);
